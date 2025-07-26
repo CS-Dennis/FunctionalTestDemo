859142237d08dd9f3b80ee24d181d8826f915389
dennis <dennis@demo.com>
2025-07-25 23:29:20 -0500

    init

---
+++ src/main/java/com/dennis/functionaltestdemo/FunctionalTestDemoApplication.java
@@ -0,0 +1,13 @@
+package com.dennis.functionaltestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionalTestDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunctionalTestDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dennis/functionaltestdemo/FunctionalTestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dennis.functionaltestdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionalTestDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
